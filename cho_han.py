18fa578bcc27d04672b42aa0905215d4c261cd7f
Riley Drellishak <rileydrellishak@gmail.com>
2025-08-21 12:32:19 -0400

    Tests for function logics

--- cho_han.py
+++ cho_han.py
@@ -43,19 +43,21 @@ def validate_bet(bet):
         return True
 
 def validate_prediction(prediction):
-    if not prediction.isalpha():
-        return False
+    if (
+        (prediction.lower() == "cho")
+        or (prediction.lower() == "han")
+    ):
+        return True
     else:
-        if prediction != "cho" or prediction != "han":
-            return False
-        else:
-            return True
-    
+        print("Please enter CHO if you think the sum will be even,"
+            f"or HAN if you think the sum will be odd.")
+        return False
+
 def get_user_prediction():
-    valid_input = False
-    while not valid_input:
-        prediction = input("CHO (even) or HAN (odd)? ").lower()
-        valid_input = validate_prediction(prediction)
+    prediction_received = False
+    while not prediction_received:
+        prediction = input("CHO (even) or HAN (odd)? ")
+        prediction_received = validate_prediction(prediction)
     return prediction
 
 def dice_roll():
@@ -71,6 +73,22 @@ def sum_roll(die_1, die_2):
 
 def even_or_odd(sum_of_roll):
     if sum_of_roll % 2 == 0:
-        return "Cho"
+        return "cho"
     else:
-        return "Han"
+        return "han"
+    
+    
+def gameplay():
+    bet = get_user_bet()
+    prediction = get_user_prediction()
+    die_1, die_2 = dice_roll()
+    jn_1, jn_2 = announce_roll(die_1, die_2)
+    sum_of_roll = sum_roll(die_1, die_2)
+    cho_or_han = even_or_odd(sum_of_roll)
+    print(f"{jn_1} - {jn_2}")
+    print(f"{die_1} - {die_2}")
+    print(f"{sum_of_roll}")
+    print(cho_or_han)
+    print(prediction == cho_or_han)
+
+gameplay()
